601065e06c1b44af7fe9d36b086b370905a06538
Ludovic Cyril Michel <ludovic@tenatek.com>
2018-05-13 15:13:45 -0500

    added support for multiple query values on the same field

--- QueryBuilder.js
+++ QueryBuilder.js
@@ -1,7 +1,21 @@
 function attrSearch(params, query, schema) {
   for (let key in params) {
     if (schema[key]) {
-      query[key] = params[key];
+      if (typeof params[key] === 'string') {
+        query.$and.push({
+          [key]: params[key]
+        });
+      } else if (params[key].constructor === Array) {
+        let orArray = [];
+        for (let value of params[key]) {
+          orArray.push({
+            [key]: value
+          });
+        }
+        query.$and.push({
+          $or: orArray
+        });
+      }
     }
   }
 }
@@ -13,9 +27,16 @@ function textSearch(param, query) {
 }
 
 function format(params, schema) {
-  let query = {};
-  if (params._search) textSearch(params._search, query);
+  let query = {
+    $and: []
+  };
+  if (params._search) {
+    textSearch(params._search, query);
+  }
   attrSearch(params, query, schema);
+  if (query.$and.length === 0) {
+    delete query.$and;
+  }
   return query;
 }
 
