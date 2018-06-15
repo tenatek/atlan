43da669ea269e83943fe2d11f33762797446e2c2
Ludovic Cyril Michel <ludovic@tenatek.com>
2018-06-14 22:54:24 -0500

    added tests for driver

--- tests/Driver.test.js
+++ tests/Driver.test.js
@@ -1,4 +1,5 @@
 const { MongoClient } = require('mongodb');
+const { JSONPath } = require('acamani');
 
 const Driver = require('../lib/Driver');
 
@@ -10,7 +11,16 @@ beforeAll(async () => {
   connection = await MongoClient.connect(global.__MONGO_URI__);
   database = await connection.db(global.__MONGO_DB_NAME__);
   driver = new Driver(database);
-  driver.addIndex('jedi', {});
+  driver.addIndex('jedi', {
+    ref: [
+      {
+        path: new JSONPath().addPathSegment('mentor'),
+        data: {
+          ref: 'jedi'
+        }
+      }
+    ]
+  });
 });
 
 afterAll(async () => {
@@ -19,8 +29,8 @@ afterAll(async () => {
 
 test('simple insert of documents', async () => {
   await driver.insertDoc('jedi', {
-    name: 'Anakin Skywalker',
-    origin: 'Tatooine'
+    name: 'Qui-gon Jinn',
+    origin: 'Coruscant'
   });
   let docs = await database
     .collection('jedi')
@@ -28,11 +38,62 @@ test('simple insert of documents', async () => {
     .toArray();
   docs.forEach(doc => {
     delete doc._id;
+    delete doc.mentor;
   });
   expect(docs).toEqual([
+    {
+      name: 'Qui-gon Jinn',
+      origin: 'Coruscant'
+    }
+  ]);
+});
+
+test('nested insert of documents', async () => {
+  await driver.insertDoc('jedi', {
+    name: 'Anakin Skywalker',
+    origin: 'Tatooine',
+    mentor: {
+      name: 'Obi-wan Kenobi',
+      origin: 'Stewjon'
+    }
+  });
+  let docs = await database
+    .collection('jedi')
+    .find({})
+    .toArray();
+  docs.forEach(doc => {
+    delete doc._id;
+    delete doc.mentor;
+  });
+  expect(docs).toEqual([
+    {
+      name: 'Qui-gon Jinn',
+      origin: 'Coruscant'
+    },
+    {
+      name: 'Obi-wan Kenobi',
+      origin: 'Stewjon'
+    },
     {
       name: 'Anakin Skywalker',
       origin: 'Tatooine'
     }
   ]);
+});
+
+test('simple query of documents', async () => {
+  let doc = await driver.getDocs(
+    'jedi',
+    {
+      origin: 'Tatooine'
+    },
+    [],
+    []
+  );
+  delete doc._id;
+  delete doc.mentor;
+  expect(doc).toEqual({
+    name: 'Anakin Skywalker',
+    origin: 'Tatooine'
+  });
 });
